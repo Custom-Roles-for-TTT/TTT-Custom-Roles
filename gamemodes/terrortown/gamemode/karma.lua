ae90d1a907ad4879a18da511bbf4ee8ad825515a
Malivil <mlebson@outlook.com>
2021-06-07 09:06:27 -0400

    Port "fix low-karma autokick evasion" from base TTT

--- gamemodes/terrortown/gamemode/karma.lua
+++ gamemodes/terrortown/gamemode/karma.lua
@@ -286,9 +286,7 @@ function KARMA.RoundEnd()
         KARMA.RememberAll()
 
         if config.autokick:GetBool() then
-            for _, ply in ipairs(player.GetAll()) do
-                KARMA.CheckAutoKick(ply)
-            end
+            KARMA.CheckAutoKickAll()
         end
     end
 end
@@ -385,6 +383,12 @@ function KARMA.CheckAutoKick(ply)
             ply:Kick(reason)
         end
     end
+end
+
+function KARMA.CheckAutoKickAll()
+   for _, ply in ipairs(player.GetAll()) do
+      KARMA.CheckAutoKick(ply)
+   end
 end
 
 function KARMA.PrintAll(printfn)
