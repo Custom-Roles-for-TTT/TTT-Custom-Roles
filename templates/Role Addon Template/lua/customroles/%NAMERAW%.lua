c25125134f5e8cdca4d906caaa64b55071aa4814
Malivil <mlebson@outlook.com>
2021-08-29 08:18:52 -0400

    Updated template with latest changes

--- templates/Role Addon Template/lua/customroles/%NAMERAW%.lua
+++ templates/Role Addon Template/lua/customroles/%NAMERAW%.lua
@@ -19,9 +19,5 @@ ROLE.convars = {}
 RegisterRole(ROLE)
 
 if SERVER then
-    resource.AddFile("materials/vgui/ttt/icon_ROLESHORT.vmt")
-    resource.AddFile("materials/vgui/ttt/sprite_ROLESHORT.vmt")
-    resource.AddSingleFile("materials/vgui/ttt/sprite_ROLESHORT_noz.vmt")
-    resource.AddSingleFile("materials/vgui/ttt/score_ROLESHORT.png")
-    resource.AddSingleFile("materials/vgui/ttt/tab_ROLESHORT.png")
+    AddCSLuaFile()
 end
