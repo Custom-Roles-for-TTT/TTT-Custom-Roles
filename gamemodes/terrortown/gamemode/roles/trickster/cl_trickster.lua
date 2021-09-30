eeda8cb8538466de6f28fe5f93dfce3fa021dc18
Malivil <mlebson@outlook.com>
2021-09-29 21:52:07 -0400

    Added Trickster tutorial page

---
+++ gamemodes/terrortown/gamemode/roles/trickster/cl_trickster.lua
@@ -0,0 +1,13 @@
+hook.Add("TTTTutorialRoleText", "SummonerTutorialRoleText", function(role, titleLabel)
+    if role == ROLE_TRICKSTER then
+        local roleColor = ROLE_COLORS[ROLE_INNOCENT]
+        local html = "The " .. ROLE_STRINGS[ROLE_TRICKSTER] .. " is a member of the <span style='color: rgb(" .. roleColor.r .. ", " .. roleColor.g .. ", " .. roleColor.b .. ")'>innocent team</span> who can:"
+
+        roleColor = ROLE_COLORS[ROLE_TRAITOR]
+        html = html .. "<ul style='position: relative; top: -15px;'>"
+            html = html .. "<li>See and use <span style='color: rgb(" .. roleColor.r .. ", " .. roleColor.g .. ", " .. roleColor.b .. ")'>traitor buttons</span>"
+            html = html .. "<li>See and use <span style='color: rgb(" .. roleColor.r .. ", " .. roleColor.g .. ", " .. roleColor.b .. ")'>traitor traps</span>"
+            html = html .. "<li>Loot credits from corpses"
+        return html .. "</ul>"
+    end
+end)
