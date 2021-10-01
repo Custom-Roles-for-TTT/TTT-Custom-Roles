7c5aea01aef949049c5f4174e144217881402839
Malivil <mlebson@outlook.com>
2021-10-01 16:44:53 -0400

    Added Zombie tutorial page

--- gamemodes/terrortown/gamemode/roles/zombie/cl_zombie.lua
+++ gamemodes/terrortown/gamemode/roles/zombie/cl_zombie.lua
@@ -182,6 +182,45 @@ end)
 
 hook.Add("TTTTutorialRoleText", "Zombie_TTTTutorialRoleText", function(role, titleLabel)
     if role == ROLE_ZOMBIE then
-        return ""
+        -- Use this for highlighting things like "brains"
+        local traitorColor = ROLE_COLORS[ROLE_TRAITOR]
+        local roleTeam = player.GetRoleTeam(ROLE_ZOMBIE, true)
+        local roleTeamString, roleTeamColor = GetRoleTeamInfo(roleTeam, true)
+
+        local html = "The " .. ROLE_STRINGS[ROLE_ZOMBIE] .. " is a member of the <span style='color: rgb(" .. roleTeamColor.r .. ", " .. roleTeamColor.g .. ", " .. roleTeamColor.b .. ")'>" .. roleTeamString .. "</span> team that uses their claws to attack their enemies."
+
+        -- Convert
+        html = html .. "<span style='display: block; margin-top: 10px;'>Killing a player with their claws will <span style='color: rgb(" .. traitorColor.r .. ", " .. traitorColor.g .. ", " .. traitorColor.b .. ")'>turn the target</span> into a " .. ROLE_STRINGS[ROLE_ZOMBIE] .. " thrall.</span>"
+
+        -- Leap
+        if GetGlobalBool("ttt_zombie_leap_enable", true) then
+            html = html .. "<span style='display: block; margin-top: 10px;'>By using the secondary attack with the claws, " .. ROLE_STRINGS_PLURAL[ROLE_ZOMBIE] .. " can <span style='color: rgb(" .. traitorColor.r .. ", " .. traitorColor.g .. ", " .. traitorColor.b .. ")'>leap in the air</span> to surprise their potential targets.</span>"
+        end
+
+        -- Spit
+        if GetGlobalBool("ttt_zombie_spit_enable", true) then
+            local keyMappingStyles = "font-size: 12px; color: black; display: inline-block; padding: 0px 3px; height: 16px; border-width: 4px; border-style: solid; border-left-color: rgb(221, 221, 221); border-bottom-color: rgb(119, 119, 102); border-right-color: rgb(119, 119, 119); border-top-color: rgb(255, 255, 255); background-color: rgb(204, 204, 187);"
+            html = html .. "<span style='display: block; margin-top: 10px;'>If the target is out of range of the claws, the " .. ROLE_STRINGS[ROLE_ZOMBIE] .. " can <span style='color: rgb(" .. traitorColor.r .. ", " .. traitorColor.g .. ", " .. traitorColor.b .. ")'>spit acid</span> at them by pressing the "
+
+            local key = Key("+reload", "R")
+            html = html .. "<span style='" .. keyMappingStyles .. "'>" .. key .. "</span> key.</span>"
+        end
+
+        -- Vision
+        local hasVision = GetGlobalBool("ttt_zombie_vision_enable", false)
+        if hasVision then
+            html = html .. "<span style='display: block; margin-top: 10px;'>Their <span style='color: rgb(" .. traitorColor.r .. ", " .. traitorColor.g .. ", " .. traitorColor.b .. ")'>hunger for brains</span> helps them see their targets through walls by highlighting their enemies.</span>"
+        end
+
+        -- Target ID
+        if GetGlobalBool("ttt_zombie_show_target_icon", false) then
+            html = html .. "<span style='display: block; margin-top: 10px;'>Their targets can"
+            if hasVision then
+                html = html .. " also"
+            end
+            html = html .. " be identified by the <span style='color: rgb(" .. traitorColor.r .. ", " .. traitorColor.g .. ", " .. traitorColor.b .. ")'>KILL</span> icon floating over their heads.</span>"
+        end
+
+        return html
     end
 end)
