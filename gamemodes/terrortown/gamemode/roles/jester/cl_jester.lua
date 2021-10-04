10dd6e5a70f6af0967cbe54cf98f5d0707c905c8
Malivil <mlebson@outlook.com>
2021-10-04 15:11:41 -0400

    Added Jester tutorial

--- gamemodes/terrortown/gamemode/roles/jester/cl_jester.lua
+++ gamemodes/terrortown/gamemode/roles/jester/cl_jester.lua
@@ -45,7 +45,13 @@ end)
 hook.Add("TTTTutorialRoleText", "Jester_TTTTutorialRoleText", function(role, titleLabel)
     if role == ROLE_JESTER then
         local roleColor = GetRoleTeamColor(ROLE_TEAM_JESTER)
-        return "The " .. ROLE_STRINGS[ROLE_JESTER] .. " is a <span style='color: rgb(" .. roleColor.r .. ", " .. roleColor.g .. ", " .. roleColor.b .. ")'>jester</span> role whose goal is to be."
-        -- TODO
+        local html =  "The " .. ROLE_STRINGS[ROLE_JESTER] .. " is a <span style='color: rgb(" .. roleColor.r .. ", " .. roleColor.g .. ", " .. roleColor.b .. ")'>jester</span> role whose goal is to be killed by another player."
+
+        if GetGlobalBool("ttt_jester_win_by_traitors", false) then
+            local traitorColor = ROLE_COLORS[ROLE_TRAITOR]
+            html = html .. "<span style='display: block; margin-top: 10px;'>Be careful! Jesters <span style='text-decoration: underline'>DO NOT</span> win if they are killed by a member of the <span style='color: rgb(" .. traitorColor.r .. ", " .. traitorColor.g .. ", " .. traitorColor.b .. ")'>traitor team!</span>.</span>"
+        end
+
+        return html
     end
 end)
