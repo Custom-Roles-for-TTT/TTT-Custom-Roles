e6604f47b2b92bf23392d554a62dbd9d00b22242
NoxxFlame <noxxflame@gmail.com>
2021-10-05 21:07:40 +1100

    Preparation for 1.3.0 release

--- gamemodes/terrortown/gamemode/shared.lua
+++ gamemodes/terrortown/gamemode/shared.lua
@@ -1,5 +1,5 @@
 -- Version string for display and function for version checks
-CR_VERSION = "1.2.8"
+CR_VERSION = "1.3.0"
 
 function CRVersion(version)
     local installedVersionRaw = string.Split(CR_VERSION, ".")
