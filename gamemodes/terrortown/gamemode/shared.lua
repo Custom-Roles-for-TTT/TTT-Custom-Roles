e3e4e0271825975139d8516cceb696a042c507fa
Malivil <mlebson@outlook.com>
2021-12-15 12:01:26 -0500

    Maintain non-beta on release branch

--- gamemodes/terrortown/gamemode/shared.lua
+++ gamemodes/terrortown/gamemode/shared.lua
@@ -18,7 +18,7 @@ local StringSub = string.sub
 
 -- Version string for display and function for version checks
 CR_VERSION = "1.4.3"
-CR_BETA = true
+CR_BETA = false
 
 function CRVersion(version)
     local installedVersionRaw = StringSplit(CR_VERSION, ".")
