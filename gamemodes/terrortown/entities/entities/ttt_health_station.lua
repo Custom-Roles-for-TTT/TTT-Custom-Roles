1beae91c6f6c6f2ebb1bc92ff6d8945d4d83f4dc
Malivil <mlebson@outlook.com>
2022-03-16 07:35:08 -0400

    Standardized spacing

--- gamemodes/terrortown/entities/entities/ttt_health_station.lua
+++ gamemodes/terrortown/entities/entities/ttt_health_station.lua
@@ -44,49 +44,49 @@ function ENT:SetupDataTables()
 end
 
 function ENT:Initialize()
-   self:SetModel(self.Model)
+    self:SetModel(self.Model)
 
-   self:PhysicsInit(SOLID_VPHYSICS)
-   self:SetMoveType(MOVETYPE_VPHYSICS)
-   self:SetSolid(SOLID_BBOX)
+    self:PhysicsInit(SOLID_VPHYSICS)
+    self:SetMoveType(MOVETYPE_VPHYSICS)
+    self:SetSolid(SOLID_BBOX)
 
-   local b = 32
-   self:SetCollisionBounds(Vector(-b, -b, -b), Vector(b,b,b))
+    local b = 32
+    self:SetCollisionBounds(Vector(-b, -b, -b), Vector(b,b,b))
 
-   self:SetCollisionGroup(COLLISION_GROUP_WEAPON)
-   if SERVER then
-      self:SetMaxHealth(200)
+    self:SetCollisionGroup(COLLISION_GROUP_WEAPON)
+    if SERVER then
+        self:SetMaxHealth(200)
 
-      local phys = self:GetPhysicsObject()
-      if IsValid(phys) then
-         phys:SetMass(200)
-      end
+        local phys = self:GetPhysicsObject()
+        if IsValid(phys) then
+            phys:SetMass(200)
+        end
 
-      self:SetUseType(CONTINUOUS_USE)
-   end
-   self:SetHealth(200)
+        self:SetUseType(CONTINUOUS_USE)
+    end
+    self:SetHealth(200)
 
-   self:SetColor(Color(180, 180, 250, 255))
+    self:SetColor(Color(180, 180, 250, 255))
 
-   self:SetStoredHealth(200)
+    self:SetStoredHealth(200)
 
-   self:SetPlacer(nil)
+    self:SetPlacer(nil)
 
-   self.NextHeal = 0
+    self.NextHeal = 0
 
-   self.fingerprints = {}
+    self.fingerprints = {}
 end
 
 
 function ENT:AddToStorage(amount)
-   self:SetStoredHealth(math.min(self.MaxStored, self:GetStoredHealth() + amount))
+    self:SetStoredHealth(math.min(self.MaxStored, self:GetStoredHealth() + amount))
 end
 
 function ENT:TakeFromStorage(amount)
-   -- if we only have 5 healthpts in store, that is the amount we heal
-   amount = math.min(amount, self:GetStoredHealth())
-   self:SetStoredHealth(math.max(0, self:GetStoredHealth() - amount))
-   return amount
+    -- if we only have 5 healthpts in store, that is the amount we heal
+    amount = math.min(amount, self:GetStoredHealth())
+    self:SetStoredHealth(math.max(0, self:GetStoredHealth() - amount))
+    return amount
 end
 
 local healsound = Sound("items/medshot4.wav")
@@ -94,83 +94,83 @@ local failsound = Sound("items/medshotno1.wav")
 
 local last_sound_time = 0
 function ENT:GiveHealth(ply, max_heal)
-   if self:GetStoredHealth() > 0 then
-      max_heal = max_heal or self.MaxHeal
-      local dmg = ply:GetMaxHealth() - ply:Health()
-      if dmg > 0 then
-         -- constant clamping, no risks
-         local healed = self:TakeFromStorage(math.min(max_heal, dmg))
-         local new = math.min(ply:GetMaxHealth(), ply:Health() + healed)
+    if self:GetStoredHealth() > 0 then
+        max_heal = max_heal or self.MaxHeal
+        local dmg = ply:GetMaxHealth() - ply:Health()
+        if dmg > 0 then
+            -- constant clamping, no risks
+            local healed = self:TakeFromStorage(math.min(max_heal, dmg))
+            local new = math.min(ply:GetMaxHealth(), ply:Health() + healed)
 
-         ply:SetHealth(new)
-         hook.Run("TTTPlayerUsedHealthStation", ply, self, healed)
+            ply:SetHealth(new)
+            hook.Run("TTTPlayerUsedHealthStation", ply, self, healed)
 
-         if last_sound_time + 2 < CurTime() then
-            self:EmitSound(healsound)
-            last_sound_time = CurTime()
-         end
+            if last_sound_time + 2 < CurTime() then
+                self:EmitSound(healsound)
+                last_sound_time = CurTime()
+            end
 
-         if not table.HasValue(self.fingerprints, ply) then
-            table.insert(self.fingerprints, ply)
-         end
+            if not table.HasValue(self.fingerprints, ply) then
+                table.insert(self.fingerprints, ply)
+            end
 
-         return true
-      else
-         self:EmitSound(failsound)
-      end
-   else
-      self:EmitSound(failsound)
-   end
+            return true
+        else
+            self:EmitSound(failsound)
+        end
+    else
+        self:EmitSound(failsound)
+    end
 
-   return false
+    return false
 end
 
 function ENT:Use(ply)
-   if IsValid(ply) and ply:IsPlayer() and ply:IsActive() then
-      local t = CurTime()
-      if t > self.NextHeal then
-         local healed = self:GiveHealth(ply, self.HealRate)
+    if IsValid(ply) and ply:IsPlayer() and ply:IsActive() then
+        local t = CurTime()
+        if t > self.NextHeal then
+            local healed = self:GiveHealth(ply, self.HealRate)
 
-         self.NextHeal = t + (self.HealFreq * (healed and 1 or 2))
-      end
-   end
+            self.NextHeal = t + (self.HealFreq * (healed and 1 or 2))
+        end
+    end
 end
 
 if SERVER then
-   -- recharge
-   local nextcharge = 0
-   function ENT:Think()
-      if nextcharge < CurTime() then
-         self:AddToStorage(self.RechargeRate)
+    -- recharge
+    local nextcharge = 0
+    function ENT:Think()
+        if nextcharge < CurTime() then
+            self:AddToStorage(self.RechargeRate)
 
-         nextcharge = CurTime() + self.RechargeFreq
-      end
-   end
+            nextcharge = CurTime() + self.RechargeFreq
+        end
+    end
 
-   local ttt_damage_own_healthstation = CreateConVar("ttt_damage_own_healthstation", "0") -- 0 as detective cannot damage their own health station
-	
-   -- traditional equipment destruction effects
-   function ENT:OnTakeDamage(dmginfo)
-      if dmginfo:GetAttacker() == self:GetPlacer() and not ttt_damage_own_healthstation:GetBool() then return end
-   
-      self:TakePhysicsDamage(dmginfo)
+    local ttt_damage_own_healthstation = CreateConVar("ttt_damage_own_healthstation", "0") -- 0 as detective cannot damage their own health station
 
-      self:SetHealth(self:Health() - dmginfo:GetDamage())
+    -- traditional equipment destruction effects
+    function ENT:OnTakeDamage(dmginfo)
+        if dmginfo:GetAttacker() == self:GetPlacer() and not ttt_damage_own_healthstation:GetBool() then return end
 
-      local att = dmginfo:GetAttacker()
-      local placer = self:GetPlacer()
-      if IsPlayer(att) then
-         DamageLog(Format("DMG: \t %s [%s] damaged health station [%s] for %d dmg", att:Nick(), att:GetRoleString(),  (IsPlayer(placer) and placer:Nick() or "<disconnected>"), dmginfo:GetDamage()))
-      end
+        self:TakePhysicsDamage(dmginfo)
 
-      if self:Health() < 0 then
-         self:Remove()
+        self:SetHealth(self:Health() - dmginfo:GetDamage())
 
-         util.EquipmentDestroyed(self:GetPos())
+        local att = dmginfo:GetAttacker()
+        local placer = self:GetPlacer()
+        if IsPlayer(att) then
+            DamageLog(Format("DMG: \t %s [%s] damaged health station [%s] for %d dmg", att:Nick(), att:GetRoleString(), IsPlayer(placer) and placer:Nick() or "<disconnected>", dmginfo:GetDamage()))
+        end
 
-         if IsValid(self:GetPlacer()) then
-            LANG.Msg(self:GetPlacer(), "hstation_broken")
-         end
-      end
-   end
+        if self:Health() < 0 then
+            self:Remove()
+
+            util.EquipmentDestroyed(self:GetPos())
+
+            if IsValid(self:GetPlacer()) then
+                LANG.Msg(self:GetPlacer(), "hstation_broken")
+            end
+        end
+    end
 end
