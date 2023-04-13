c64ecef0336ea95e31a0317469eec6b046bb8242
Malivil <mlebson@outlook.com>
2023-04-13 11:33:57 -0400

    Reduce global pollution

--- gamemodes/terrortown/gamemode/cl_scoring.lua
+++ gamemodes/terrortown/gamemode/cl_scoring.lua
@@ -301,7 +301,7 @@ local function GetWinTitle(wintype)
     return hook.Call("TTTScoringWinTitleOverride", nil, wintype, wintitles, title) or title
 end
 
-function GetFontForWinTitle(wintxt, width)
+local function GetFontForWinTitle(wintxt, width)
     -- Scale the title down if it's too wide
     local winfont_options = {"WinHuge", "WinLarge", "WinMedium", "WinSmall", "WinTiny"}
     local winfont = "WinHuge"
@@ -852,8 +852,8 @@ function CLSCORE:BuildHilitePanel(dpanel)
 
     local bg = vgui.Create("ColoredBox", dpanel)
     bg:SetColor(Color(50, 50, 50, 255))
-    bg:SetSize(w,h)
-    bg:SetPos(0,0)
+    bg:SetSize(w, h)
+    bg:SetPos(0, 0)
 
     local winlbl = vgui.Create("DLabel", dpanel)
     local wintxt = PT(title.txt, title.params or {})
