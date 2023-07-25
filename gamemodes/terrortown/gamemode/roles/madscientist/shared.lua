374cccac8a1f6b18b0ab4f20a0d884a9009637e4
Malivil <mlebson@outlook.com>
2023-07-25 11:43:22 -0400

    Updated Mad Scientist to use replicated convars

--- gamemodes/terrortown/gamemode/roles/madscientist/shared.lua
+++ gamemodes/terrortown/gamemode/roles/madscientist/shared.lua
@@ -2,22 +2,12 @@ AddCSLuaFile()
 
 local table = table
 
--- Initialize role features
-ROLE_SELECTION_PREDICATE[ROLE_MADSCIENTIST] = function()
-    -- Mad Scientist can only spawn when zombies are on their team
-    return (INDEPENDENT_ROLES[ROLE_MADSCIENTIST] and INDEPENDENT_ROLES[ROLE_ZOMBIE]) or
-            (MONSTER_ROLES[ROLE_MADSCIENTIST] and MONSTER_ROLES[ROLE_ZOMBIE])
-end
-
-hook.Add("TTTUpdateRoleState", "MadScientist_Team_TTTUpdateRoleState", function()
-    local madscientist_is_monster = GetGlobalBool("ttt_madscientist_is_monster", false)
-    MONSTER_ROLES[ROLE_MADSCIENTIST] = madscientist_is_monster
-    INDEPENDENT_ROLES[ROLE_MADSCIENTIST] = not madscientist_is_monster
-end)
-
 ------------------
 -- ROLE CONVARS --
 ------------------
+
+CreateConVar("ttt_madscientist_respawn_enable", "0", FCVAR_REPLICATED)
+local madscientist_is_monster = CreateConVar("ttt_madscientist_is_monster", "0", FCVAR_REPLICATED)
 
 ROLE_CONVARS[ROLE_MADSCIENTIST] = {}
 table.insert(ROLE_CONVARS[ROLE_MADSCIENTIST], {
@@ -33,3 +23,18 @@ table.insert(ROLE_CONVARS[ROLE_MADSCIENTIST], {
     cvar = "ttt_madscientist_is_monster",
     type = ROLE_CONVAR_TYPE_BOOL
 })
+
+-------------------
+-- ROLE FEATURES --
+-------------------
+
+ROLE_SELECTION_PREDICATE[ROLE_MADSCIENTIST] = function()
+    -- Mad Scientist can only spawn when zombies are on their team
+    return (INDEPENDENT_ROLES[ROLE_MADSCIENTIST] and INDEPENDENT_ROLES[ROLE_ZOMBIE]) or
+            (MONSTER_ROLES[ROLE_MADSCIENTIST] and MONSTER_ROLES[ROLE_ZOMBIE])
+end
+
+hook.Add("TTTUpdateRoleState", "MadScientist_Team_TTTUpdateRoleState", function()
+    MONSTER_ROLES[ROLE_MADSCIENTIST] = madscientist_is_monster:GetBool()
+    INDEPENDENT_ROLES[ROLE_MADSCIENTIST] = not madscientist_is_monster:GetBool()
+end)
