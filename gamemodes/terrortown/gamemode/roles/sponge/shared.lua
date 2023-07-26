dab8ce8a73b843717510f205071a80a92068750b
Malivil <mlebson@outlook.com>
2023-07-25 21:12:14 -0400

    Updated Sponge to use replicated convars

--- gamemodes/terrortown/gamemode/roles/sponge/shared.lua
+++ gamemodes/terrortown/gamemode/roles/sponge/shared.lua
@@ -13,6 +13,8 @@ ROLE_MAX_HEALTH[ROLE_SPONGE] = 150
 -- ROLE CONVARS --
 ------------------
 
+CreateConVar("ttt_sponge_aura_radius", "5", FCVAR_REPLICATED, "The radius of the sponge's aura in meters", 1, 30)
+
 ROLE_CONVARS[ROLE_SPONGE] = {}
 table.insert(ROLE_CONVARS[ROLE_SPONGE], {
     cvar = "ttt_sponge_aura_radius",
