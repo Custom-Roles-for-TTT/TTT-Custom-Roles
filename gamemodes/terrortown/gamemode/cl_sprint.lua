3033c266ae19a9134fa9e949cf507fde8d57ffc8
NoxxFlame <noxxflame@gmail.com>
2023-07-27 21:35:23 +1000

    Fixed crosshair size growing over time

--- gamemodes/terrortown/gamemode/cl_sprint.lua
+++ gamemodes/terrortown/gamemode/cl_sprint.lua
@@ -6,11 +6,13 @@ local crosshairSize = nil
 
 AddHook("TTTSprintStateChange", "Sprinting_Crosshair_TTTSprintStateChange", function(ply, sprinting, _)
     if ply ~= LocalPlayer() then return end
+    if sprinting and crosshairSize ~= nil then return end
 
     if sprinting then
         crosshairSize = GetConVar("ttt_crosshair_size"):GetFloat()
         RunConsoleCommand("ttt_crosshair_size", crosshairSize + 1)
     elseif crosshairSize then
         RunConsoleCommand("ttt_crosshair_size", crosshairSize)
+        crosshairSize = nil
     end
 end)
