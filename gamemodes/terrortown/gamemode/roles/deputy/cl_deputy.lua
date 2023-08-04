c67bc714373def35b425393e2b4d7304351957c9
Malivil <mlebson@outlook.com>
2023-08-03 21:29:06 -0400

    Updated deputy tutorial

--- gamemodes/terrortown/gamemode/roles/deputy/cl_deputy.lua
+++ gamemodes/terrortown/gamemode/roles/deputy/cl_deputy.lua
@@ -5,6 +5,7 @@ local hook = hook
 -------------
 
 local deputy_use_detective_icon = GetConVar("ttt_deputy_use_detective_icon")
+local deputy_damage_penalty = GetConVar("ttt_deputy_damage_penalty")
 
 ------------------
 -- TRANSLATIONS --
@@ -35,6 +36,10 @@ hook.Add("TTTTutorialRoleText", "Deputy_TTTTutorialRoleText", function(role, tit
         -- Promotion
         html = html .. "<span style='display: block; margin-top: 10px;'>After the " .. ROLE_STRINGS[ROLE_DETECTIVE] .. " is killed, <span style='color: rgb(" .. roleColor.r .. ", " .. roleColor.g .. ", " .. roleColor.b .. ")'>the " .. ROLE_STRINGS[ROLE_DEPUTY] .. " is \"promoted\"</span> and then must assume their role as the new " .. ROLE_STRINGS[ROLE_DETECTIVE] .. ".</span>"
         html = html .. "<span style='display: block; margin-top: 10px;'>They have <span style='color: rgb(" .. roleColor.r .. ", " .. roleColor.g .. ", " .. roleColor.b .. ")'>all the powers of " .. ROLE_STRINGS_EXT[ROLE_DETECTIVE] .. "</span> including " .. ROLE_STRINGS[ROLE_DETECTIVE] .. "-only weapons and the ability to search bodies.</span>"
+
+        if deputy_damage_penalty:GetFloat() > 0 then
+            html = html .. "<span style='display: block; margin-top: 10px;'>Be careful though! Before the " .. ROLE_STRINGS[ROLE_DEPUTY] .. " has been promoted, they <span style='color: rgb(" .. roleColor.r .. ", " .. roleColor.g .. ", " .. roleColor.b .. ")'>do less damage</span>.</span>"
+        end
 
         -- Icon
         html = html .. "<span style='display: block; margin-top: 10px;'>Once promoted, <span style='color: rgb(" .. roleColor.r .. ", " .. roleColor.g .. ", " .. roleColor.b .. ")'>all players</span> will see the "
