7cd84a00d1c154153a5f44b07adc9345910989fa
Malivil <mlebson@outlook.com>
2023-08-08 19:05:12 -0400

    Added shop assimilation

--- gamemodes/terrortown/gamemode/roles/hivemind/shared.lua
+++ gamemodes/terrortown/gamemode/roles/hivemind/shared.lua
@@ -1,5 +1,13 @@
 AddCSLuaFile()
 
+local hook = hook
+local player = player
+local table = table
+
+local AddHook = hook.Add
+local GetAllPlayers = player.GetAll
+local TableInsert = table.insert
+
 ------------------
 -- ROLE CONVARS --
 ------------------
@@ -9,15 +17,15 @@ CreateConVar("ttt_hivemind_friendly_fire", "0", FCVAR_REPLICATED)
 local hivemind_is_monster = CreateConVar("ttt_hivemind_is_monster", "0", FCVAR_REPLICATED)
 
 ROLE_CONVARS[ROLE_HIVEMIND] = {}
-table.insert(ROLE_CONVARS[ROLE_HIVEMIND], {
+TableInsert(ROLE_CONVARS[ROLE_HIVEMIND], {
     cvar = "ttt_hivemind_vision_enable",
     type = ROLE_CONVAR_TYPE_BOOL
 })
-table.insert(ROLE_CONVARS[ROLE_HIVEMIND], {
+TableInsert(ROLE_CONVARS[ROLE_HIVEMIND], {
     cvar = "ttt_hivemind_friendly_fire",
     type = ROLE_CONVAR_TYPE_BOOL
 })
-table.insert(ROLE_CONVARS[ROLE_HIVEMIND], {
+TableInsert(ROLE_CONVARS[ROLE_HIVEMIND], {
     cvar = "ttt_hivemind_is_monster",
     type = ROLE_CONVAR_TYPE_BOOL
 })
@@ -26,8 +34,33 @@ table.insert(ROLE_CONVARS[ROLE_HIVEMIND], {
 -- ROLE FEATURES --
 -------------------
 
-hook.Add("TTTUpdateRoleState", "HiveMind_Team_TTTUpdateRoleState", function()
+AddHook("TTTUpdateRoleState", "HiveMind_Team_TTTUpdateRoleState", function()
     local is_monster = hivemind_is_monster:GetBool()
     MONSTER_ROLES[ROLE_HIVEMIND] = is_monster
     INDEPENDENT_ROLES[ROLE_HIVEMIND] = not is_monster
+end)
+
+ROLE_SHOP_SYNC_ROLES[ROLE_HIVEMIND] = {}
+
+AddHook("TTTPlayerRoleChanged", "HiveMind_ShopSync_TTTPlayerRoleChanged", function(ply, oldRole, newRole)
+    if GetRoundState() ~= ROUND_ACTIVE then return end
+    if oldRole == ROLE_HIVEMIND or newRole ~= ROLE_HIVEMIND then return end
+    -- Don't bother checking if the player is alive since this happens right after
+    -- they die and by the time this hook is called on the client they haven't been respawned yet
+
+    -- Add any new (valid) role to the list of shop sync roles
+    if oldRole ~= ROLE_NONE and not table.HasValue(ROLE_SHOP_SYNC_ROLES[ROLE_HIVEMIND], oldRole) then
+        TableInsert(ROLE_SHOP_SYNC_ROLES[ROLE_HIVEMIND], oldRole)
+
+        -- Bust the weapons cache for all players so the weapons show in the shop and they can buy them
+        for _, p in ipairs(GetAllPlayers()) do
+            if not p:IsHiveMind() then continue end
+            -- Bust the shop cache
+            p:ConCommand("ttt_reset_weapons_cache")
+        end
+    end
+end)
+
+AddHook("TTTPrepareRound", "HiveMind_ShopSync_PrepareRound", function()
+    table.Empty(ROLE_SHOP_SYNC_ROLES[ROLE_HIVEMIND])
 end)
