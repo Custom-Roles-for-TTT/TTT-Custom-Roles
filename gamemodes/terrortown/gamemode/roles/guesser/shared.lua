16a3b3a6ac20b8c6c37808954920301c92303109
NoxxFlame <noxxflame@gmail.com>
2023-08-10 17:22:07 +1000

    Added convars to ULX

--- gamemodes/terrortown/gamemode/roles/guesser/shared.lua
+++ gamemodes/terrortown/gamemode/roles/guesser/shared.lua
@@ -1,1 +1,22 @@
 AddCSLuaFile()
+
+local table = table
+
+------------------
+-- ROLE CONVARS --
+------------------
+
+ROLE_CONVARS[ROLE_GUESSER] = {}
+table.insert(ROLE_CONVARS[ROLE_GUESSER], {
+    cvar = "ttt_guesser_can_guess_detectives",
+    type = ROLE_CONVAR_TYPE_BOOL
+})
+table.insert(ROLE_CONVARS[ROLE_GUESSER], {
+    cvar = "ttt_guesser_unguessable_roles",
+    type = ROLE_CONVAR_TYPE_TEXT
+})
+table.insert(ROLE_CONVARS[ROLE_GUESSER], {
+    cvar = "ttt_guesser_minimum_radius",
+    type = ROLE_CONVAR_TYPE_NUM,
+    decimal = 0
+})
