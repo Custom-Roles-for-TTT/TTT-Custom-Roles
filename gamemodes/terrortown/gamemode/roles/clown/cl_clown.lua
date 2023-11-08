ea80a03283f958cb449baadeaad285d70496e054
Malivil <Malivil@users.noreply.github.com>
2023-11-07 22:07:30 -0500

    Added description for Killer Changed tutorials that mention the "KILL" icon to talk about a skull icon instead

--- gamemodes/terrortown/gamemode/roles/clown/cl_clown.lua
+++ gamemodes/terrortown/gamemode/roles/clown/cl_clown.lua
@@ -135,7 +135,7 @@ hook.Add("TTTTutorialRoleText", "Clown_TTTTutorialRoleText", function(role, titl
 
         -- Target ID
         if clown_show_target_icon:GetBool() then
-            html = html .. "<span style='display: block; margin-top: 10px;'>Their targets can be identified by the <span style='color: rgb(" .. traitorColor.r .. ", " .. traitorColor.g .. ", " .. traitorColor.b .. ")'>KILL</span> icon floating over their heads.</span>"
+            html = html .. "<span style='display: block; margin-top: 10px;'>Their targets can be identified by the <span style='color: rgb(" .. traitorColor.r .. ", " .. traitorColor.g .. ", " .. traitorColor.b .. ")'>skull</span> icon floating over their heads.</span>"
         end
 
         -- Hide When Active
