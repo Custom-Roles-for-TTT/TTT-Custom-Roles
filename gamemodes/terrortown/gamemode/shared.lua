4d00aac557a8e3070b14ee8962e19ba7cb04962b
Malivil <Malivil@users.noreply.github.com>
2023-12-31 08:32:31 -0500

    Maintain beta markings

--- gamemodes/terrortown/gamemode/shared.lua
+++ gamemodes/terrortown/gamemode/shared.lua
@@ -23,7 +23,7 @@ include("player_class/player_ttt.lua")
 
 -- Version string for display and function for version checks
 CR_VERSION = "2.0.4"
-CR_BETA = false
+CR_BETA = true
 CR_WORKSHOP_ID = CR_BETA and "2404251054" or "2421039084"
 
 function CRVersion(version)
