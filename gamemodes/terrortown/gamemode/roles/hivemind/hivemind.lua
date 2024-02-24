2f60ced2602100c86c199070ff936cc520584f6a
Malivil <mlebson@outlook.com>
2024-02-24 16:50:35 -0500

    Comment typo

--- gamemodes/terrortown/gamemode/roles/hivemind/hivemind.lua
+++ gamemodes/terrortown/gamemode/roles/hivemind/hivemind.lua
@@ -97,7 +97,7 @@ end)
 AddHook("TTTBodyCreditsLooted", "HiveMind_CreditsSync_TTTBodyCreditsLooted", function(ply, deadPly, rag, credits)
     if not IsPlayer(deadPly) or not deadPly:IsHiveMind() then return end
 
-    -- Find all corpses that being to hive minds and remove their credits
+    -- Find all corpses that belong to hive minds and remove their credits
     for _, p in ipairs(GetAllPlayers()) do
         if not p:IsHiveMind() then continue end
         p:SetCredits(0)
