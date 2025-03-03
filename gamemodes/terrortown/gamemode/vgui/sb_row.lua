1ad9a3ccf3e367361c4c53dec08276d84ff6200e
Malivil <mlebson@outlook.com>
2025-03-03 10:15:56 -0500

    Updated implementation of "TTT: Close player volume slider along with scoreboard"

--- gamemodes/terrortown/gamemode/vgui/sb_row.lua
+++ gamemodes/terrortown/gamemode/vgui/sb_row.lua
@@ -21,6 +21,17 @@ local MathMin = math.min
 local MathRound = math.Round
 local MathSin = math.sin
 
+local OpenedVoicePanels = {}
+local function HideVolumePanels()
+    for _, pnl in pairs(OpenedVoicePanels) do
+        if IsValid(pnl) then
+            pnl:Close()
+            pnl = nil
+        end
+    end
+end
+hook.Add("ScoreboardHide", "TTT_HideVolumePanels", HideVolumePanels)
+
 SB_ROW_HEIGHT = 24 --16
 
 local PANEL = {}
@@ -378,7 +389,9 @@ function PANEL:SetPlayer(ply)
 
     self.voice.DoRightClick = function()
         if IsValid(ply) and ply ~= LocalPlayer() then
-            self.voice.volume = self:ShowMicVolumeSlider()
+            HideVolumePanels()
+            local voiceSlider = self:ShowMicVolumeSlider()
+            table.insert(OpenedVoicePanels, voiceSlider)
         end
      end
 
@@ -536,7 +549,7 @@ function PANEL:ShowMicVolumeSlider()
     local sliderHeight = 16
     local sliderDisplayHeight = 8
 
-    local x = MathMax(gui.MouseX() - width, 0)
+    local x = MathMax(gui.MouseX() - width - padding, 0)
     local y = MathMin(gui.MouseY(), ScrH() - height)
 
     local currentPlayerVolume = self:GetPlayer():GetVoiceVolumeScale()
@@ -554,6 +567,7 @@ function PANEL:ShowMicVolumeSlider()
     frame.Paint = function(s, w, h)
         draw.RoundedBox(5, 0, 0, w, h, Color(24, 25, 28, 255))
     end
+    frame.Player = self:GetPlayer()
 
     -- Automatically close after 10 seconds (something may have gone wrong)
     timer.Simple(10, function() if IsValid(frame) then frame:Close() end end)
@@ -574,7 +588,7 @@ function PANEL:ShowMicVolumeSlider()
     slider:SetSlideX(currentPlayerVolume)
     slider:SetLockY(0.5)
     slider.TranslateValues = function(s, sx, sy)
-        if IsValid(self:GetPlayer()) then self:GetPlayer():SetVoiceVolumeScale(sx) end
+        if IsValid(frame.Player) then frame.Player:SetVoiceVolumeScale(x) end
         return sx, sy
     end
 
@@ -617,19 +631,5 @@ function PANEL:ShowMicVolumeSlider()
 
     return frame
 end
-
-local function HideVolumePanels()
-    if not IsValid(sboard_panel) then return end
-
-    for _, grp in pairs(sboard_panel.ply_groups) do
-        for _, ply in pairs(grp.rows) do
-            if IsValid(ply.voice.volume) then
-               ply.voice.volume:Close()
-               ply.voice.volume = nil
-            end
-        end
-    end
-end
-hook.Add("ScoreboardHide", "TTT_HideVolumePanels", HideVolumePanels)
 
 vgui.Register("TTTScorePlayerRow", PANEL, "DButton")
