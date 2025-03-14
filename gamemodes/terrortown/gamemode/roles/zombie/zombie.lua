d8a20e706c151a646cbf4958fbf69d1ad271655f
Logan <lobsterlogan43@yahoo.com>
2025-03-14 14:28:39 -0500

    Update gamemodes/terrortown/gamemode/roles/zombie/zombie.lua
    Co-authored-by: Malivil <Malivil@users.noreply.github.com>

--- gamemodes/terrortown/gamemode/roles/zombie/zombie.lua
+++ gamemodes/terrortown/gamemode/roles/zombie/zombie.lua
@@ -364,7 +364,7 @@ hook.Add("DoPlayerDeath", "Zombie_DoPlayerDeath", function(victim, attacker, dmg
     if not IsValid(inflictor) or WEPS.GetClass(inflictor) ~= "weapon_zom_claws" then return end
 
     -- If they were killed by a normal slash attack or spit conversion is enabled and they were killed by spitting, convert them
-    if not attacker:IsRoleAbilityDisabled() and (dmginfo:IsDamageType(DMG_SLASH) or (zombie_spit_convert:GetBool() and dmginfo:IsDamageType(DMG_BULLET))) then
+    if (dmginfo:IsDamageType(DMG_SLASH) or (zombie_spit_convert:GetBool() and dmginfo:IsDamageType(DMG_BULLET))) and not attacker:IsRoleAbilityDisabled() then
         attacker:AddCredits(1)
         LANG.Msg(attacker, "credit_all", { role = ROLE_STRINGS[ROLE_ZOMBIE], num = 1 })
         hook.Call("TTTPlayerRoleChangedByItem", nil, attacker, victim, inflictor)
