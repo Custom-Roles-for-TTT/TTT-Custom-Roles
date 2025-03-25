dc33ec9a2cd5d91d45c093e9f2a05c76974da41e
Malivil <Malivil@users.noreply.github.com>
2025-03-25 16:22:06 -0400

    Fixed good twin not having their invulnerability skipped when they are disabled

--- gamemodes/terrortown/gamemode/roles/twins/twins.lua
+++ gamemodes/terrortown/gamemode/roles/twins/twins.lua
@@ -186,6 +186,8 @@ local function CheckTwinsInvulnerability(ply, oldRole)
         invulnerabilityTriggered = true
         if #livingGoodTwins > 0 then
             for _, p in ipairs(livingGoodTwins) do
+                if p:IsRoleAbilityDisabled() then continue end
+
                 p:QueueMessage(MSG_PRINTBOTH, "The " .. ROLE_STRINGS[ROLE_EVILTWIN] .. " has died! You have been granted " .. invulnerability_timer .. " seconds of invulnerability.")
                 p:SetInvulnerable(true, true)
                 p:SetNWFloat("TTTTwinsInvulnerabilityEnd", CurTime() + invulnerability_timer)
