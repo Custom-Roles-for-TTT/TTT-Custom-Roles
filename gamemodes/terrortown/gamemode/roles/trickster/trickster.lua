dcaca652e80261ac30c56abf43b380e6fc1e028f
Malivil <Malivil@users.noreply.github.com>
2025-03-27 22:04:00 -0400

    Fixed error with trickster trying to use traitor buttons

--- gamemodes/terrortown/gamemode/roles/trickster/trickster.lua
+++ gamemodes/terrortown/gamemode/roles/trickster/trickster.lua
@@ -2,7 +2,7 @@ local FindEntsByClass = ents.FindByClass
 
 ROLE_SELECTION_PREDICATE[ROLE_TRICKSTER] = function() return #FindEntsByClass("ttt_traitor_button") > 0 end
 
-hook.Add("TTTCanUseTraitorButton", "Trickster_Disabled_TTTCanUseTraitorButton", function(ply)
+hook.Add("TTTCanUseTraitorButton", "Trickster_Disabled_TTTCanUseTraitorButton", function(ent, ply)
     if ply:IsActiveTrickster() and ply:IsRoleAbilityDisabled() then
         return false
     end
