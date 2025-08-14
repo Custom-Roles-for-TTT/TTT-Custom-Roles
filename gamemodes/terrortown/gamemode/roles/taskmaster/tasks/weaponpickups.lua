dc628b525ec8f90d227fa4d9a2306270f5c91c6b
Malivil <mlebson@outlook.com>
2025-08-14 17:51:33 -0400

    Added Pick Up Weapons task

---
+++ gamemodes/terrortown/gamemode/roles/taskmaster/tasks/weaponpickups.lua
@@ -0,0 +1,61 @@
+local table = table
+
+local TASK = {}
+
+TASK.id = "weaponpickups"
+
+local taskmaster_weaponpickups_count = CreateConVar("ttt_taskmaster_weaponpickups_count", "20", FCVAR_REPLICATED, "The number of unique weapons a player must pick up to complete the 'Pick Up Weapons' task", 1, 240)
+table.insert(ROLE_CONVARS[ROLE_TASKMASTER], {
+    cvar = "ttt_taskmaster_weaponpickups_count",
+    type = ROLE_CONVAR_TYPE_NUM,
+    decimal = 0
+})
+
+TASK.Name = function(ply)
+    local count = taskmaster_weaponpickups_count:GetInt()
+    local name = "Pick Up " .. count .. " Weapon"
+    if count ~= 1 then
+        name = name .. "s"
+    end
+    return name
+end
+
+TASK.Description = function(ply)
+    local count = taskmaster_weaponpickups_count:GetInt()
+    local desc = "Pick up " .. count
+    if count == 1 then
+        return desc .. "weapon"
+    end
+    return desc .. " unique weapons from the ground"
+end
+
+if SERVER then
+    TASK.CanAssignTask = function(ply)
+        return true
+    end
+
+    TASK.RequiredFeatures = {}
+
+    TASK.OnTaskAssigned = function(ply)
+        local count = taskmaster_weaponpickups_count:GetInt()
+        local total = 0
+        hook.Add("WeaponEquip", "Taskmaster_WeaponPickup_WeaponEquip_" .. ply:SteamID64(), function(wep, owner)
+            if owner ~= ply then return end
+            if not IsValid(wep) then return end
+            if wep.Task_WeaponPickupsUsed then return end
+            wep.Task_WeaponPickupsUsed = true
+            total = total + 1
+            if total >= count then
+                ply:CompleteTask(TASK.id)
+            end
+        end)
+    end
+
+    TASK.OnTaskRemoved = function(ply)
+        hook.Remove("WeaponEquip", "Taskmaster_WeaponPickup_WeaponEquip_" .. ply:SteamID64())
+    end
+
+    TASK.OnTaskComplete = TASK.OnTaskRemoved
+end
+
+TASKMASTER.RegisterTask(TASK)
