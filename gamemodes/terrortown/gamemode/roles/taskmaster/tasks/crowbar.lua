4add9c4b8ade3587b1d3db300306128f7ec7ef09
Malivil <mlebson@outlook.com>
2025-08-14 17:41:12 -0400

    Added Crowbar task

---
+++ gamemodes/terrortown/gamemode/roles/taskmaster/tasks/crowbar.lua
@@ -0,0 +1,91 @@
+local table = table
+
+local TASK = {}
+
+TASK.id = "crowbar"
+
+local taskmaster_crowbar_times = CreateConVar("ttt_taskmaster_crowbar_times", "50", FCVAR_REPLICATED, "The number of times a player must use their crowbar to complete the 'Swing Crowbar' task", 1, 240)
+table.insert(ROLE_CONVARS[ROLE_TASKMASTER], {
+    cvar = "ttt_taskmaster_crowbar_times",
+    type = ROLE_CONVAR_TYPE_NUM,
+    decimal = 0
+})
+
+TASK.Name = function(ply)
+    local times = taskmaster_crowbar_times:GetInt()
+    local name = "Swing Crowbar " .. times .. " Time"
+    if times ~= 1 then
+        name = name .. "s"
+    end
+    return name
+end
+
+TASK.Description = function(ply)
+    local times = taskmaster_crowbar_times:GetInt()
+    local desc = "Use the crowbar weapon " .. times .. " time"
+    if times ~= 1 then
+        desc = desc .. "s"
+    end
+    return desc
+end
+
+if SERVER then
+    TASK.CanAssignTask = function(ply)
+        return true
+    end
+
+    TASK.RequiredFeatures = {}
+
+    local function SetupPrimaryAttack(ply, wep)
+        if wep.Task_CrowbarPrimaryAttack then return end
+
+        local times = taskmaster_crowbar_times:GetInt()
+        local total = 0
+        wep.Task_CrowbarPrimaryAttack = wep.PrimaryAttack
+        wep.PrimaryAttack = function(this, worldsnd)
+            wep.Task_CrowbarPrimaryAttack(wep, worldsnd)
+            total = total + 1
+            if total >= times then
+                ply:CompleteTask(TASK.id)
+            end
+        end
+    end
+
+    local function ClearPrimaryAttack(wep)
+        if not wep.Task_CrowbarPrimaryAttack then return end
+        wep.PrimaryAttack = wep.Task_CrowbarPrimaryAttack
+        wep.Task_CrowbarPrimaryAttack = nil
+    end
+
+    TASK.OnTaskAssigned = function(ply)
+        local crowbar = ply:GetWeapon("weapon_zm_improvised")
+        if IsValid(crowbar) then
+            SetupPrimaryAttack(ply, crowbar)
+        end
+
+        local sid64 = ply:SteamID64()
+        hook.Add("WeaponEquip", "Taskmaster_Crowbar_WeaponEquip_" .. sid64, function(wep, owner)
+            if owner ~= ply then return end
+            if WEPS.GetClass(wep) ~= "weapon_zm_improvised" then return end
+            SetupPrimaryAttack(ply, wep)
+        end)
+
+        hook.Add("PlayerDroppedWeapon", "Taskmaster_Crowbar_PlayerDroppedWeapon_" .. sid64, function( owner, wep)
+            if owner ~= ply then return end
+            if WEPS.GetClass(wep) ~= "weapon_zm_improvised" then return end
+            ClearPrimaryAttack(wep)
+        end)
+    end
+
+    TASK.OnTaskRemoved = function(ply)
+        ClearPrimaryAttack(ply)
+
+        local sid64 = ply:SteamID64()
+        hook.Remove("WeaponEquip", "Taskmaster_Crowbar_WeaponEquip_" .. sid64)
+        hook.Remove("PlayerDroppedWeapon", "Taskmaster_Crowbar_PlayerDroppedWeapon_" .. sid64)
+    end
+
+    TASK.OnTaskComplete = TASK.OnTaskRemoved
+end
+
+TASKMASTER.RegisterTask(TASK)
