504ef06dab16f8dcda7ccda5e02b53a43bed415e
Malivil <mlebson@outlook.com>
2025-08-14 13:28:36 -0400

    Added Pick Up Shop Item task

---
+++ gamemodes/terrortown/gamemode/roles/taskmaster/tasks/pickupshopitem.lua
@@ -0,0 +1,36 @@
+local TASK = {}
+
+TASK.id = "pickupshopitem"
+
+TASK.Name = function(ply)
+    return "Pick Up Shop Item"
+end
+
+TASK.Description = function(ply)
+    return "Pick up a shop item dropped on the ground"
+end
+
+if SERVER then
+    TASK.CanAssignTask = function(ply)
+        return true
+    end
+
+    TASK.RequiredFeatures = {}
+
+    TASK.OnTaskAssigned = function(ply)
+        hook.Add("WeaponEquip", "Taskmaster_PickUpShopItem_WeaponEquip_" .. ply:SteamID64(), function(wep, caller)
+            if not IsPlayer(caller) then return end
+            if caller ~= ply then return end
+            if not IsValid(wep) or not wep.CanBuy or #wep.CanBuy == 0 then return end
+            ply:CompleteTask(TASK.id)
+        end)
+    end
+
+    TASK.OnTaskRemoved = function(ply)
+        hook.Remove("WeaponEquip", "Taskmaster_PickUpShopItem_WeaponEquip_" .. ply:SteamID64())
+    end
+
+    TASK.OnTaskComplete = TASK.OnTaskRemoved
+end
+
+TASKMASTER.RegisterTask(TASK)
