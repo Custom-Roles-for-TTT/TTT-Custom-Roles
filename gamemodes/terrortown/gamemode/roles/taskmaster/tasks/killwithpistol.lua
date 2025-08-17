39c7e9a5257df0c45c1b0086671bfa243f5efbe5
donaldsonnick <noxxflame@gmail.com>
2025-08-17 09:04:34 +0100

    Added 'Kill Kill a Player With a Pistol' task

---
+++ gamemodes/terrortown/gamemode/roles/taskmaster/tasks/killwithpistol.lua
@@ -0,0 +1,41 @@
+local hook = hook
+
+local TASK = {}
+
+TASK.id = "killwithpistol"
+TASK.isKillTask = true
+
+TASK.Name = function(ply)
+    return "Kill a Player With a Pistol"
+end
+
+TASK.Description = function(ply)
+    return "Kill another player using a pistol"
+end
+
+if SERVER then
+    TASK.CanAssignTask = function(ply)
+        return true
+    end
+
+    TASK.RequiredFeatures = {}
+
+    TASK.OnTaskAssigned = function(ply)
+        hook.Add("PlayerDeath", "Taskmaster_KillWithPistol_PlayerDeath_" .. ply:SteamID64(), function(victim, inflictor, attacker)
+            if not IsPlayer(victim) then return end
+            if not IsPlayer(attacker) or not attacker:IsActiveTaskmaster() or attacker ~= ply then return end
+
+            if IsValid(inflictor) and inflictor:IsWeapon() and inflictor.Kind == WEAPON_PISTOL then
+                ply:CompleteTask(TASK.id)
+            end
+        end)
+    end
+
+    TASK.OnTaskRemoved = function(ply)
+        hook.Remove("PlayerDeath", "Taskmaster_KillWithPistol_PlayerDeath_" .. ply:SteamID64())
+    end
+
+    TASK.OnTaskComplete = TASK.OnTaskRemoved
+end
+
+TASKMASTER.RegisterTask(TASK)
