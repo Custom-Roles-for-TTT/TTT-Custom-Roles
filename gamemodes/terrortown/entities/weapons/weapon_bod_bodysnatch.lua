ce3823dc671151012153596f56a6eed7c1d97197
Malivil <mlebson@outlook.com>
2026-01-31 22:21:24 -0500

    Added ability for Bodysnatcher to have the roles they can snatch restricted by team-based convars

--- gamemodes/terrortown/entities/weapons/weapon_bod_bodysnatch.lua
+++ gamemodes/terrortown/entities/weapons/weapon_bod_bodysnatch.lua
@@ -30,11 +30,16 @@ SWEP.Kind = WEAPON_ROLE
 
 SWEP.FindRespawnLocation = false
 
-if SERVER then
-    SWEP.DeviceTimeConVar = CreateConVar("ttt_bodysnatcher_device_time", "5", FCVAR_NONE, "The amount of time (in seconds) the bodysnatcher's device takes to use", 0, 60)
-end
+local target_innocents = CreateConVar("ttt_bodysnatcher_target_innocents", "1", FCVAR_REPLICATED, "whether the bodysnatcher can target innocent bodies", 0,  1)
+local target_detectives = CreateConVar("ttt_bodysnatcher_target_detectives", "1", FCVAR_REPLICATED, "whether the bodysnatcher can target detective bodies", 0,  1)
+local target_traitors = CreateConVar("ttt_bodysnatcher_target_traitors", "1", FCVAR_REPLICATED, "whether the bodysnatcher can target traitor bodies", 0,  1)
+local target_jesters = CreateConVar("ttt_bodysnatcher_target_jesters", "1", FCVAR_REPLICATED, "whether the bodysnatcher can target jester bodies", 0,  1)
+local target_independents = CreateConVar("ttt_bodysnatcher_target_independents", "1", FCVAR_REPLICATED, "whether the bodysnatcher can target independent bodies", 0,  1)
+local target_monsters = CreateConVar("ttt_bodysnatcher_target_monsters", "1", FCVAR_REPLICATED, "whether the bodysnatcher can target monster bodies", 0,  1)
 
 if SERVER then
+    SWEP.DeviceTimeConVar = CreateConVar("ttt_bodysnatcher_device_time", "5", FCVAR_NONE, "The amount of time (in seconds) the bodysnatcher's device takes to use", 0, 60)
+
     util.AddNetworkString("TTT_Bodysnatched")
     util.AddNetworkString("TTT_ScoreBodysnatch")
     util.AddNetworkString("TTT_BodysnatchUpdateCorpseRole")
@@ -186,6 +191,18 @@ if SERVER then
         SetRoleMaxHealth(owner)
 
         SendFullStateUpdate()
+    end
+
+    function SWEP:ValidateTarget(ply, body, bone)
+        if (ply:IsInnocentTeam() and not ply:IsDetectiveTeam() and not target_innocents:GetBool()) or
+            (ply:IsDetectiveTeam() and not target_detectives:GetBool()) or
+            (ply:IsTraitorTeam() and not target_traitors:GetBool()) or
+            (ply:IsJesterTeam() and not target_jesters:GetBool()) or
+            (ply:IsIndependentTeam() and not target_independents:GetBool()) or
+            (ply:IsMonsterTeam() and not target_monsters:GetBool()) then
+            return false, "INVALID TARGET ROLE"
+        end
+        return true, ""
     end
 
     function SWEP:GetProgressMessage(ply, body, bone)
