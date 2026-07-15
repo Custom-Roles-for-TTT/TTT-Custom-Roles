00c38f7be7d219ce34bd9ecce7bb97fced6d1d1f
Malivil <mlebson@outlook.com>
2026-07-15 13:52:08 -0400

    Updated role registration update script to handle more file formats

--- scripts/role_register_update.py
+++ scripts/role_register_update.py
@@ -5,9 +5,71 @@ from collections import defaultdict
 
 rootdir = input("Path to roles folder: ")
 
-pattern = re.compile(r"^(?:hook\.Add|AddHook)\(\"(.+)\", \"(.+)\", function\((.*)\)", flags=re.MULTILINE)
-named_pattern = re.compile(r"^(?:hook\.Add|AddHook)\(\"(.+)\", \"(.+)\", (?!function)(.*)\)", flags=re.MULTILINE)
+pattern = re.compile(r"(?:hook\.Add|AddHook)\(\"(.+)\", \"(.+)\", function\((.*)\)", flags=re.MULTILINE)
+named_pattern = re.compile(r"(?:hook\.Add|AddHook)\(\"(.+)\", \"(.+)\", (?!function)(.*)\)", flags=re.MULTILINE)
+space_pattern = re.compile(r"\s*")
 substitution = "local function \\2(\\3)"
+
+def file_output(fileHandle, line, newline = False):
+    if fileHandle != None:
+        fileHandle.write(line)
+        if newline:
+            fileHandle.write("\n")
+    elif newline:
+        print(line)
+    else:
+        print(line, end='')
+
+def write_hooks(file, isRole, hooks, lastLine, fileHandle, lineSpaces = ""):
+    if len(hooks) == 0:
+        return lastLine
+
+    if len(lastLine) > 0:
+        if not lastLine.isspace():
+            file_output(fileHandle, "\n")
+        if not lastLine.endswith("\n"):
+            file_output(fileHandle, "\n")
+
+    role = os.path.splitext(file)[0].removeprefix("cl_").removeprefix("sh_").upper()
+
+    file_output(fileHandle, lineSpaces + "------------------", True)
+    file_output(fileHandle, lineSpaces + "-- REGISTRATION --", True)
+    file_output(fileHandle, lineSpaces + "------------------\n", True)
+
+    if isRole:
+        file_output(fileHandle, lineSpaces + "ROLE.registeredhooks = {", True)
+    else:
+        file_output(fileHandle, lineSpaces + "ROLE_REGISTERED_HOOKS[ROLE_" + role + "] = {", True)
+    keys = list(hooks.keys())
+    keys.sort()
+    lastKey = keys[len(keys) - 1]
+    for key in keys:
+        handlers = list(hooks[key].keys())
+        handlers.sort()
+        if len(handlers) > 1:
+            lastHandler = handlers[len(handlers) - 1]
+            file_output(fileHandle, lineSpaces + "    [\"" + key + "\"] = {")
+            for handlerName in handlers:
+                fnName = handlerName
+                if hooks[key][handlerName] != None:
+                    fnName = hooks[key][handlerName]
+                file_output(fileHandle, lineSpaces + "        [\"" + handlerName + "\"] = " + fnName + "")
+                if handlerName != lastHandler:
+                    file_output(fileHandle, ",")
+                file_output(fileHandle, "\n")
+            file_output(fileHandle, lineSpaces + "    }")
+        else:
+            handlerName = handlers[0]
+            fnName = handlerName
+            if hooks[key][handlerName] != None:
+                fnName = hooks[key][handlerName]
+            file_output(fileHandle, lineSpaces + "    [\"" + key + "\"] = " + fnName)
+
+        if key != lastKey:
+            file_output(fileHandle, ",")
+        file_output(fileHandle, "\n")
+    file_output(fileHandle, lineSpaces + "}")
+    return "!PLACEHOLDER!"
 
 for subdir, dirs, files in os.walk(rootdir):
     for file in files:
@@ -24,14 +86,22 @@ for subdir, dirs, files in os.walk(rootdir):
         lastLine = None
         isRole = False
         skipped = 0
+        lineSpaces = ""
+        inScope = False
+        scopeSpaces = None
+
+        # Check if this file registers a role because we handle the hooks differently
+        with open(os.path.join(subdir, file)) as f:
+            if "RegisterRole(ROLE)" in f.read():
+                isRole = True
 
         print("Processing " + path)
         for line in fileinput.input(path, inplace=True):
             namedMatch = False
             matches = []
-            if pattern.match(line) != None:
+            if pattern.search(line) != None:
                 matches = pattern.finditer(line)
-            elif named_pattern.match(line) != None:
+            elif named_pattern.search(line) != None:
                 namedMatch = True
                 matches = named_pattern.finditer(line)
 
@@ -54,16 +124,40 @@ for subdir, dirs, files in os.walk(rootdir):
                     else:
                         hooks[hookName][hookId] = None
 
+            # If this is the registration line we want to skip it and the next (assumingly blank) line
             if line.startswith("RegisterRole(ROLE)"):
-                isRole = True
                 skipNext = True
             else:
+                # If we're in a SERVER or CLIENT scope
+                if inScope:
+                    # Save the first line within it's spaces so we know how
+                    # many spaces to add to the hook mapping in this scope
+                    if scopeSpaces == None:
+                        space_match = space_pattern.match(line)
+                        if space_match:
+                            scopeSpaces = space_match.group()
+                    # And we're ending the scope, print out the hooks that belong to it
+                    if line == "end\n" or line == "end":
+                        lastLine = write_hooks(file, isRole, hooks, lastLine, None, scopeSpaces)
+                        print("")
+                        hooks = {}
+                        scopeSpaces = None
+                        inScope = False
+
                 if replace:
                     didReplace = True
                     print(pattern.sub(substitution, line), end='')
-                elif didReplace and (line == "end)\n" or line == "end)"):
+
+                    # If this hook definition starts with spaces, save the amount so we know
+                    # how much to indent the end line
+                    space_match = space_pattern.match(line)
+                    if space_match:
+                        lineSpaces = space_match.group()
+                # If we're ending a hook definition, remove the ending paren
+                elif didReplace and (line == lineSpaces + "end)\n" or line == lineSpaces + "end)"):
                     didReplace = False
-                    print("end")
+                    print(lineSpaces + "end")
+                    lineSpaces = ""
                 elif skipNext or namedMatch:
                     skipNext = False
                     if line.isspace():
@@ -71,55 +165,13 @@ for subdir, dirs, files in os.walk(rootdir):
                     else:
                         line = "!PLACEHOLDER!\n"
                 else:
+                    if line.startswith("if SERVER then") or line.startswith("if CLIENT then"):
+                        inScope = True
                     print(line, end='')
                 lastLine = line
 
         with open(os.path.join(subdir, file), "a") as f:
-            if len(hooks) > 0:
-                if len(lastLine) > 0:
-                    if not lastLine.isspace():
-                        f.write("\n")
-                    if not lastLine.endswith("\n"):
-                        f.write("\n")
-
-                role = os.path.splitext(file)[0].removeprefix("cl_").removeprefix("sh_")
-                title_role = role.title()
-
-                f.write("------------------\n")
-                f.write("-- REGISTRATION --\n")
-                f.write("------------------\n\n")
-
-                f.write("ROLE.registeredhooks = {\n")
-                keys = list(hooks.keys())
-                keys.sort()
-                lastKey = keys[len(keys) - 1]
-                for key in keys:
-                    handlers = list(hooks[key].keys())
-                    handlers.sort()
-                    if len(handlers) > 1:
-                        lastHandler = handlers[len(handlers) - 1]
-                        f.write("    [\"" + key + "\"] = {")
-                        for handlerName in handlers:
-                            fnName = handlerName
-                            if hooks[key][handlerName] != None:
-                                fnName = hooks[key][handlerName]
-                            f.write("        [\"" + handlerName + "\"] = " + fnName + "")
-                            if handlerName != lastHandler:
-                                f.write(",")
-                            f.write("\n")
-                        f.write("    }")
-                    else:
-                        handlerName = handlers[0]
-                        fnName = handlerName
-                        if hooks[key][handlerName] != None:
-                            fnName = hooks[key][handlerName]
-                        f.write("    [\"" + key + "\"] = " + fnName)
-
-                    if key != lastKey:
-                        f.write(",")
-                    f.write("\n")
-                f.write("}")
-                lastLine = "!PLACEHOLDER!"
+            lastLine = write_hooks(file, isRole, hooks, lastLine, f)
             if isRole:
                 if not lastLine.endswith("\n"):
                     f.write("\n")
